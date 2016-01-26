4c9b0059492d266b04fcfc8cb1a3a673187a2066
MOSCATO Antonio <amoscato@ecritel.net>
2016-01-26 11:08:25 +0100

    add RecordSession

--- RecordSession/recordsession.sh
+++ RecordSession/recordsession.sh
@@ -4,9 +4,10 @@ SESSION_PATH=/root/sessions/
 
 if (( $UID == 0 )); then
 	if [ ! -d $SESSION_PATH ] ; then
-		 mkdir $SESSION_PATH
-	else
-		echo "Unable to create $SESSION_PATH" 
+		echo "Create $SESSION_PATH" 
+		if ! mkdir $SESSION_PATH ; then
+			echo "Unable to create $SESSION_PATH" 
+		fi
 	fi
 	script -a ${SESSION_PATH}script-$(date +%Hh%M-%d-%m-%y).out
 fi
